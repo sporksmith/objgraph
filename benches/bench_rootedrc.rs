617524ac7de28685d03170fa875855654d15c866
Jim Newsome <jnewsome@torproject.org>
2022-05-14 18:29:27 -0500

    Iterate on benchmarks; I think Rc::clone was getting optimized away

--- benches/bench_rootedrc.rs
+++ benches/bench_rootedrc.rs
@@ -1,34 +1,27 @@
+#![feature(thread_local)]
+
 use std::{rc::Rc, sync::Arc};
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use objgraph::{Root, RootedRc};
 
-fn do_clone<T: Clone>(iterations: usize, val: T) {
-    for _ in 0..black_box(iterations) {
-        let _ = val.clone();
-    }
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("RootedRc::clone 1000", |b| {
-        b.iter(|| {
-            let root = Root::new(());
-            let _lock = root.lock();
-            let x = RootedRc::new(root.tag(), 0);
-            do_clone(black_box(1000), x);
-        })
+    let root = Root::new(());
+    let _lock = root.lock();
+
+    let mut group = c.benchmark_group("clone");
+    group.bench_function("RootedRc", |b| {
+        b.iter_batched(
+            || RootedRc::new(root.tag(), ()),
+            |x| x.clone(),
+            BatchSize::SmallInput,
+        );
     });
-    c.bench_function("Arc::clone 1000", |b| {
-        b.iter(|| {
-            let x = Arc::new(());
-            do_clone(black_box(1000), x);
-        })
+    group.bench_function("Arc", |b| {
+        b.iter_batched(|| Arc::new(()), |x| x.clone(), BatchSize::SmallInput);
     });
-    c.bench_function("Rc::clone 1000", |b| {
-        b.iter(|| {
-            let x = Rc::new(());
-            do_clone(black_box(1000), x);
-        })
+    group.bench_function("Rc", |b| {
+        b.iter_batched(|| Rc::new(()), |x| x.clone(), BatchSize::SmallInput);
     });
 }
 
