ec27129b719caf4731ef91590fa4828c7f4f36cd
Jim Newsome <jnewsome@torproject.org>
2022-05-14 18:34:40 -0500

    Add drop benchmark

--- benches/bench_rootedrc.rs
+++ benches/bench_rootedrc.rs
@@ -9,20 +9,39 @@ fn criterion_benchmark(c: &mut Criterion) {
     let root = Root::new(());
     let _lock = root.lock();
 
-    let mut group = c.benchmark_group("clone");
-    group.bench_function("RootedRc", |b| {
-        b.iter_batched(
-            || RootedRc::new(root.tag(), ()),
-            |x| x.clone(),
-            BatchSize::SmallInput,
-        );
-    });
-    group.bench_function("Arc", |b| {
-        b.iter_batched(|| Arc::new(()), |x| x.clone(), BatchSize::SmallInput);
-    });
-    group.bench_function("Rc", |b| {
-        b.iter_batched(|| Rc::new(()), |x| x.clone(), BatchSize::SmallInput);
-    });
+    {
+        let mut group = c.benchmark_group("clone");
+        group.bench_function("RootedRc", |b| {
+            b.iter_batched(
+                || RootedRc::new(root.tag(), ()),
+                |x| x.clone(),
+                BatchSize::SmallInput,
+            );
+        });
+        group.bench_function("Arc", |b| {
+            b.iter_batched(|| Arc::new(()), |x| x.clone(), BatchSize::SmallInput);
+        });
+        group.bench_function("Rc", |b| {
+            b.iter_batched(|| Rc::new(()), |x| x.clone(), BatchSize::SmallInput);
+        });
+    }
+
+    {
+        let mut group = c.benchmark_group("drop");
+        group.bench_function("RootedRc", |b| {
+            b.iter_batched(
+                || RootedRc::new(root.tag(), ()),
+                |x| drop(x),
+                BatchSize::SmallInput,
+            );
+        });
+        group.bench_function("Arc", |b| {
+            b.iter_batched(|| Arc::new(()), |x| drop(x), BatchSize::SmallInput);
+        });
+        group.bench_function("Rc", |b| {
+            b.iter_batched(|| Rc::new(()), |x| drop(x), BatchSize::SmallInput);
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
